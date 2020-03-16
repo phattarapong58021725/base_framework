f037a1c5255d236a10945e48240d505233e14615
phattarapong58021725 <32901174+phattarapong58021725@users.noreply.github.com>
2020-03-16 15:20:57 +0700

    fix

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Chart } from 'chart.js';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/_service/user.service.';
 import { Setting } from 'src/app/_setting/setting';
+import { User } from 'src/app/_model/user';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -12,9 +13,13 @@ export class DashboardComponent implements OnInit{
   public id: string;
   public authorities: any = null;
   public role :any = null;
+  user: User = null;
   constructor(private route: ActivatedRoute
           , public userSV: UserService) { 
+    this.user = new User;
     this.authorities = this.userSV.currentUserValue.authorities;
+    this.user.fullName = this.userSV.currentUserValue.fullName;
+    this.user.organizeDesc = this.userSV.currentUserValue.organizeDesc;
     this.role = Setting.ROLE;
   }
 
